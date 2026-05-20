8233f5b2496998b5bb40219eca88ed05752d45be
AbdiFitaah <c.fitaax2019@gmail.com>
2026-05-20 10:51:20 -0400

    exercises

---
+++ js-exercise/exe1-5/main.js
@@ -0,0 +1,57 @@
+let x= 7;
+let y=3
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+
+
+// Exercise 2 
+let a=15;
+let b=15;
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a<b);
+console.log(a>b);
+console.log(a>=b);
+console.log(a<=b);
+
+
+// exercise 3
+
+let isSunny=true;
+let isweekend=true;
+
+console.log(isSunny && isweekend);
+
+
+// exercise 4
+
+///fucntion declaration
+function add(a,b){
+    return a+b;
+}
+console.log(add(2,3));
+console.log(add(5,3));
+
+/// fucntion of expresion
+const expressAdd =  function(a,b){
+    return a+b
+}
+
+console.log(expressAdd(2,3));
+console.log(expressAdd(5,3));
+
+
+
+/// exercise 5
+const arowAdd =()=>{
+    return a+b
+}
+
+console.log(arowAdd(2,3));
+console.log(arowAdd(5,3));
+
