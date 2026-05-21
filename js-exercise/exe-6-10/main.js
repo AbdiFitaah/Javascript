06c26bf9f948ede41450c31535d91c0263cf6834
AbdiFitaah <c.fitaax2019@gmail.com>
2026-05-21 05:17:49 -0400

    exercise 6-10

---
+++ js-exercise/exe-6-10/main.js
@@ -0,0 +1,49 @@
+
+
+// exercise 6
+let colors=['black','white','yellow'];
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
+// exercise 7
+
+colors.push('green');
+console.log(colors);
+
+color.shift()
+console.log(colors);
+
+
+// exercise 8
+
+
+let car = {
+    make: 'Toyota',
+    model: 'honda',
+    year:1997,
+
+    // exercise 9
+    start:function(){
+        console.log('The car has strarted');
+    }
+}
+console.log(car.model);
+console.log(car.year);
+console.log(car.make);
+
+
+// exercise 9 waxaa method kusoo qoray object inta wakusoo daabacay
+    car.start()
+
+
+
+    
+// exercise 10
+let library=[
+    {title:'rich',author:'hamouda',year:2000},
+    {title:'think',author:'abdifatah',year:1990},
+]
+
+console.log(library[0].title);
+console.log(library[1].author);
